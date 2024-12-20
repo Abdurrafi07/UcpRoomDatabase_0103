0ca8d60eee5d479a08af080c3b96b7fd209e1434
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-12-20 19:44:43 +0700

    Membuat Database DosenKuliah

---
+++ app/src/main/java/com/example/ucp2/data/database/DosenKuliah.kt
@@ -0,0 +1,35 @@
+package com.example.ucp2.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.dao.MataKuliahDao
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.data.entity.MataKuliah
+
+// Mendefinisikan database dengan tabel Mahasiswa
+@Database(entities = [Dosen::class,MataKuliah::class], version = 1, exportSchema = false)
+abstract class DosenKuliah : RoomDatabase(){
+
+    abstract fun mahasiswaDao(): DosenDao
+    abstract fun matakuliahDao(): MataKuliahDao
+
+    companion object{
+        // Memastikan bahwa nilai variabel Instance selalu sama di semua thread
+        @Volatile
+        private var Instance: DosenKuliah? = null
+
+        fun getDatabase(context: Context): DosenKuliah{
+            return (Instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context,
+                    DosenKuliah::class.java, // Class database
+                    "DosenKuliah" // Nama database
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
+}
