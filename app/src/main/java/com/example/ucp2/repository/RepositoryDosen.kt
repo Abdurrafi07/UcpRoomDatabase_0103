7dd40bd302a5469f5e7aa53b2f9f317b522e02e4
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-12-20 19:56:46 +0700

    Membuat RepositoryDosen dan RepositoryMataKuliah

---
+++ app/src/main/java/com/example/ucp2/repository/RepositoryDosen.kt
@@ -0,0 +1,9 @@
+package com.example.ucp2.repository
+
+import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryDosen {
+    fun getAllDosen(): Flow<List<Dosen>>
+    suspend fun insertDosen(dosen: Dosen)
+}
