20523680fa940992982949e8ca5fedc388da5de7
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-12-21 14:13:44 +0700

    Membuat HomeDosenViews

---
+++ app/src/main/java/com/example/ucp2/ui/Views/HomeDosenViews.kt
@@ -0,0 +1,242 @@
+package com.example.ucp2.ui.Views
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.ui.ViewModel.HomeDosenViewModel
+import com.example.ucp2.ui.ViewModel.HomeUiStateDosen
+import com.example.ucp2.ui.ViewModel.PenyediaViewModelDosenKuliah
+import com.example.ucp2.ui.customWidget.TopAppBar
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun HomeDosenViews(
+    viewModel: HomeDosenViewModel = viewModel(factory = PenyediaViewModelDosenKuliah.Factory),
+    onAddDosen : ()-> Unit = { },
+    onBack: () -> Unit,
+    onDetailClick : (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Dosen",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddDosen,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Dosen",
+                )
+            }
+        }
+    ) {
+            innerPadding ->
+        val HomeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeDosenViews(
+            homeUiState = HomeUiState,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun BodyHomeDosenViews(
+    homeUiState: HomeUiStateDosen,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } // SnackBar State
+    when {
+        homeUiState.isLoading -> {
+            // Menampilkan indikator loading
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            // Menampilkan Pesan Error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+
+        else -> {
+            // Menampilkan daftar Dosen
+            ListDosen(
+                listDosen = homeUiState.listDosen,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun ListDosen(
+    listDosen: List<Dosen>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+) {
+    LazyColumn (
+        modifier = modifier
+    ) {
+        items(
+            items = listDosen,
+            itemContent = { dosen ->
+                CardDosen(
+                    dosen = dosen,
+                    onClick = { onClick(dosen.nidn)}
+                )
+            }
+        )
+    }
+}
+
+
+@Composable
+fun CardDosen(
+    dosen: Dosen,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    Card(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .shadow(8.dp, shape = MaterialTheme.shapes.medium), // Adding shadow for elevation
+        shape = MaterialTheme.shapes.medium, // Rounded corners
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFE8F5E9) // Light green background for the card
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp) // Increased padding for better spacing
+        ) {
+            // Card content: Row for Dosen's Name
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Person,
+                    contentDescription = "",
+                    modifier = Modifier.size(28.dp) // Icon size
+                )
+                Spacer(modifier = Modifier.width(8.dp)) // Space between icon and text
+                Text(
+                    text = dosen.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.onSurface // Color for text
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp)) // Spacer between rows
+
+            // Row for NIDN
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.AccountBox,
+                    contentDescription = "",
+                    modifier = Modifier.size(28.dp) // Icon size
+                )
+                Spacer(modifier = Modifier.width(8.dp)) // Space between icon and text
+                Text(
+                    text = dosen.nidn,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp)) // Spacer between rows
+
+            // Row for Gender
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Face,
+                    contentDescription = "",
+                    modifier = Modifier.size(28.dp) // Icon size
+                )
+                Spacer(modifier = Modifier.width(8.dp)) // Space between icon and text
+                Text(
+                    text = dosen.jenisKelamin,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        }
+    }
+}
